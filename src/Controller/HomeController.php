275d09c7101aaa3d16e2333fd3aef15bf98e082e
Jonas Poli <jonaspoli@gmail.com>
2023-02-16 16:19:08 -0300

    Cache System

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,19 +2,28 @@
 
 namespace App\Controller;
 
+use Psr\Cache\CacheItemInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Contracts\Cache\CacheInterface;
 use Symfony\Contracts\HttpClient\HttpClientInterface;
+
 
 class HomeController extends AbstractController
 {
 
     #[Route(path:'/', name: 'app_home')]
-    public function home(HttpClientInterface $httpClient): Response
+    public function home(HttpClientInterface $httpClient, CacheInterface $cache): Response
     {
-        $response = $httpClient->request('GET', 'https://raw.githubusercontent.com/JonasPoli/array-news/main/arrayCategoryNews.json');
-        $categories = $response->toArray();
+        $categories = $cache->get('news_data', function (CacheItemInterface $cacheItem) use ($httpClient){
+            $cacheItem->expiresAfter(5);
+            $response = $httpClient->request('GET', 'https://raw.githubusercontent.com/JonasPoli/array-news/main/arrayCategoryNews.json');
+            return $response->toArray();
+        });
+
+
+
 
         $pageTitle = "Sistema de Notícias";
         return $this->render('home.html.twig', [
