4cc417c598e904cb8f3352db174caa605e299ce1
Jonas Poli <jonaspoli@gmail.com>
2023-02-23 10:01:06 -0300

    Usuários

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -3,14 +3,27 @@
 namespace App\DataFixtures;
 
 use App\Entity\NewsCategory;
+use App\Entity\User;
 use App\Factory\NewsCategoryFactory;
 use App\Factory\NewsFactory;
+use App\Factory\UserFactory;
 use App\Repository\NewsCategoryRepository;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
+use Faker\Factory;
+use Faker\Generator;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 
 class AppFixtures extends Fixture
 {
+    private Generator $faker;
+
+    public function __construct(
+        Private UserPasswordHasherInterface $passwordHasher,
+    )
+    {
+        $this->faker = Factory::create('pt_BR');
+    }
 
     public function load(ObjectManager $manager): void
     {
@@ -21,6 +34,33 @@ class AppFixtures extends Fixture
 
         ];
         });
+
+        $user = new User();
+        $user->setEmail('jonaspoli@gmail.com');
+        $user->setRoles(['ROLE_USER','ROLE_ADMIN']);
+        $manager->persist($user);
+
+        $plaintextPassword = 'password';
+        $hashedPassword = $this->passwordHasher->hashPassword(
+            $user,
+            $plaintextPassword
+        );
+        $user->setPassword($hashedPassword);
+
+
+        for ($i=0;$i<10;$i++){
+            $user = new User();
+            $user->setEmail($this->faker->email());
+            $user->setRoles(['ROLE_USER']);
+            $manager->persist($user);
+
+            $plaintextPassword = $this->faker->word();
+            $hashedPassword = $this->passwordHasher->hashPassword(
+                $user,
+                $plaintextPassword
+            );
+            $user->setPassword($hashedPassword);
+        }
         $manager->flush();
 
     }
