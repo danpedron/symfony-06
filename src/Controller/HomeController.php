0fffc3b8695cf472ed204374115bdfccd3768fdf
Jonas Poli <jonaspoli@gmail.com>
2023-03-01 00:24:25 -0300

    Inicio da aula30

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,7 +2,9 @@
 
 namespace App\Controller;
 
+use App\Service\NewsService;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Contracts\HttpClient\HttpClientInterface;
@@ -11,33 +13,24 @@ class HomeController extends AbstractController
 {
 
     #[Route(path:'/', name: 'app_home')]
-    public function home(HttpClientInterface $httpClient): Response
+    public function home(NewsService $service): Response
     {
-        $response = $httpClient->request('GET', 'https://raw.githubusercontent.com/JonasPoli/array-news/main/arrayCategoryNews.json');
-        $categories = $response->toArray();
 
         $pageTitle = "Sistema de Notícias";
         return $this->render('home.html.twig', [
             'pageTitle' => $pageTitle,
-            'categories' => $categories,
+            'categories' => $service->getCategoyList(),
         ]);
     }
 
     #[Route(path:'/category/{slug}', name: 'app_category_list')]
-    public function category($slug, HttpClientInterface $httpClient): Response
+    public function category($slug, NewsService $service): Response
     {
-        $response = $httpClient->request('GET','https://raw.githubusercontent.com/JonasPoli/array-news/6592605d783b39aa2edac63868959ded7ef700ec/arrayNews.json');
-        $news = $response->toArray();
-
-        $response = $httpClient->request('GET', 'https://raw.githubusercontent.com/JonasPoli/array-news/main/arrayCategoryNews.json');
-        $categories = $response->toArray();
-
         $pageTitle = $slug;
-
         return $this->render('category.html.twig', [
             'pageTitle' => $pageTitle,
-            'categories' => $categories,
-            'news' => $news,
+            'categories' => $service->getCategoyList(),
+            'news' => $service->getNewsList(),
         ]);
     }
 
