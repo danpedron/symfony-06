9aeda5fbcfd270ac3d86c6a12c3e93e62257ebdb
Jonas Poli <jonaspoli@gmail.com>
2023-03-01 19:15:04 -0300

    Aula39

--- src/Controller/NewsController.php
+++ src/Controller/NewsController.php
@@ -2,6 +2,8 @@
 
 namespace App\Controller;
 
+use App\Entity\News;
+use Doctrine\ORM\EntityManagerInterface;
 use Psr\Log\LoggerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
@@ -29,5 +31,19 @@ class NewsController extends AbstractController
             'noticia' => $id,
         ]);
         return $this->json($new);
+    }
+
+    #[Route('news/new')]
+    public function new(EntityManagerInterface $entityManager):Response
+    {
+        $rand = rand(18,30);
+        $news = new News();
+        $news->setTitle('Jovem de '.$rand.' anos recebe um prêmio');
+        $news->setDescription('Um jovem brasileiro de '.$rand.' anos recebeu um prêmio na Suécia');
+
+        $entityManager->persist($news);
+        $entityManager->flush();
+
+        return new Response('<h1>Notícia Criada</h1>em:'.$news->getCreateAt()->format('d/m/Y h:i:s'));
     }
 }
