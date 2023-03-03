cd9e081eb20065bc1219ab547476c917b894fae8
Jonas Poli <jonaspoli@gmail.com>
2023-03-03 00:06:04 -0300

    Aula50

--- src/Service/NewsRepository.php
+++ src/Service/NewsRepository.php
@@ -5,23 +5,20 @@ namespace App\Service;
 use Psr\Cache\CacheItemInterface;
 use Symfony\Contracts\Cache\CacheInterface;
 use Symfony\Contracts\HttpClient\HttpClientInterface;
-use Symfony\Component\DependencyInjection\Attribute\Autowire;
 
 class NewsRepository
 {
     public function __construct(
         private HttpClientInterface $httpClient,
-        private CacheInterface $cache,
-        #[Autowire('%kernel.debug%')]
-        private bool $isDebug
-    )
+        private CacheInterface $cache)
     {
+
     }
 
     public function findAll(): array
     {
         $news = $this->cache->get('news_data', function (CacheItemInterface $cacheItem) {
-            $cacheItem->expiresAfter($this->isDebug ? 5 : 60);
+            $cacheItem->expiresAfter(5);
             $response = $this->httpClient->request('GET', 'https://raw.githubusercontent.com/JonasPoli/array-news/6592605d783b39aa2edac63868959ded7ef700ec/arrayNews.json');
             return $response->toArray();
         });
