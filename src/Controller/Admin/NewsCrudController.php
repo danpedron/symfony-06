899f6f9d32a1f0119260cbca4e4b7382498c008f
Jonas Poli <jonaspoli@gmail.com>
2023-03-03 21:14:55 -0300

    Aula57

--- src/Controller/Admin/NewsCrudController.php
+++ src/Controller/Admin/NewsCrudController.php
@@ -10,6 +10,7 @@ use EasyCorp\Bundle\EasyAdminBundle\Field\ArrayField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\AssociationField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\DateTimeField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\ImageField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\IntegerField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextareaField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
@@ -44,10 +45,15 @@ class NewsCrudController extends AbstractCrudController
             IdField::new('id')->hideOnForm(),
             AssociationField::new('category')->setFormTypeOptions(['choice_label'=>'title','choice_value'=> 'id'] )->setLabel('Categoria'),
             TextField::new('title')->setLabel('Título'),
-            TextField::new('image')->setLabel('URL da Imagem'),
             TextareaField::new('content')->hideOnIndex()->setLabel('Conteúdo')->setFormType(CKEditorType::class),
             DateTimeField::new('createdAt')->setLabel('Criada em')->setFormTypeOption('disabled','disabled'),
             TextField::new('slug')->setLabel('Slug')->setFormTypeOption('disabled','disabled')->hideOnIndex(),
+            ImageField::new('image')
+                ->setLabel('Imagem da notícia')
+                ->setBasePath('uploads/images')
+                ->setUploadDir('public/uploads/images')
+                ->setUploadedFileNamePattern('[slug]-[timestamp].[extension]')
+                ->setRequired(false),
 
         ];
     }
